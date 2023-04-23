d5bb419717f8914b5af2b576694d78739438b08c
Calvin Das <calvindas7@gmail.com>
2023-04-23 20:34:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        #D developer portfolio
+      </div>
+    </>
+  )
+}
+
+export default App
